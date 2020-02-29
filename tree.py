be5e976a9e2f7d5f4076640b02c7a2062dc0536d
Gregory Erik <gregoryerikadams@outlook.com>
2020-02-29 16:26:28 +0000

    Simple temp solution to only check visible files

--- tree.py
+++ tree.py
@@ -1,30 +1,9 @@
 import os
 import json
-import time
-
-class Scanner:
-
-    def __init__(self):
-        self.flags = []
-
-    def add_flags(self):
-        flags = input("Enter flags: split by '/': ").split("/")
-        for flag in flags:
-            self.findLink(flag)
-
-    def findLink(self, flag):
-        print(f"[FL] Finding flag {flag}...", end="")
-
-        with open("links.json", "r") as json_link:
-            data = json.load(json_link)
-
-        if flag in data:
-            print("flag found!")
-            self.flags.append(data[flag]["link"])
-        else:
-            print("no flag :(")
+import sys
 
 class Counter:
+
     def __init__(self):
         Counter.count = 0
 
@@ -34,68 +13,116 @@ class Counter:
     def getCount(self):
         print("Count: ", Counter.count)
 
+class Tree:
+
+    def __init__(self, root_dir, hidden_allowed, output_data):
+        self.root = root_dir
+        self.indent = 0
+        self.c = Counter()
+        self.hidden_allowed = hidden_allowed
+        self.output_data = output_data
+
+        print("Running...\n")
+        self.run()
+        self.c.getCount()
+         
+
+    def run(self):
+        
+        contents = os.listdir(self.root)
+        if not self.hidden_allowed:
+            contents = self.remove_hidden(contents)
+
+        if len(contents) > 0:
+            for i in contents:
+                Branch(self.root, i, self.indent, self.c, self.hidden_allowed, self.output_data)
+        else:
+            data = f"+ {self.root}"
+            self.output(data)
+            #self.counter.getCount()
+            self.c.increment()
+
+    def remove_hidden(self, l):
+        return [i for i in l if not i.startswith(".")]
+    
+    def output(self, data):
+        if self.output_data:
+            print(data)
+
+    
+
 class Branch:
-    def __init__(self, root_dir, branch_dir, indent, counter):
+
+    def __init__(self, root_dir, branch_dir, indent, counter, hidden_allowed, output_data):
        self.root = root_dir
        self.branch_dir = branch_dir
        self.indent = indent
        self.counter = counter
+       self.hidden_allowed = hidden_allowed
+       self.output_data = output_data
        self.run()
         
     def run(self):
         new_path = os.path.join(self.root, self.branch_dir)
         try:
             contents = os.listdir(new_path)
-            print((" " * self.indent) + "()", self.branch_dir)
+            if not self.hidden_allowed:
+                contents = self.remove_hidden(contents)
+
+            data = " " * self.indent + f"() {self.branch_dir}"
+            self.output(data)
             #self.counter.getCount()
             self.counter.increment()
              
             if len(contents) > 0:
                 for i in contents:
-                    time.sleep(0.005)
-                    Branch(new_path, i, self.indent + 1, self.counter)
+                    Branch(new_path, i, self.indent + 1, self.counter, self.hidden_allowed, self.output_data)
 
         except:
-            print((" " * self.indent) + "+", self.branch_dir)
+            data = " " * self.indent + f"+ {self.branch_dir}"
+            self.output(data)
             #self.counter.getCount()
             self.counter.increment()
 
+    def remove_hidden(self, l):
+        return [i for i in l if not i.startswith(".")]
 
-class Tree:
-    def __init__(self, root_dir):
-        self.root = root_dir
-        self.indent = 0
-        self.c = Counter()
+    def output(self, data):
+        if self.output_data:
+            print(data)
 
-        print("Running...\n")
-        self.run()
-        #self.c.getCount()
-         
-    def run(self):
-        
-        contents = os.listdir(self.root)
-        if len(contents) > 0:
-            for i in contents:
-                Branch(self.root, i, self.indent, self.c)
-        else:
-            print("+", self.root)
-            #self.counter.getCount()
-            self.c.increment()
- 
-def run_application():
-    print("\n", os.getcwd(), "\n")
+def confirmed_input(prompt, acceptable_values):
+	user_answer = input(prompt)
+	acceptable_values = [av.lower() for av in acceptable_values]
+
+	while user_answer.lower() not in acceptable_values:
+		print(f"Answer is not in {acceptable_values}")
+		user_answer = input(prompt)
+	return user_answer
+
+def main():
+    print("\nCurrent:", os.getcwd(), "\n")
+    output_data = True
+
+    args = sys.argv
+    if len(args) > 1:
+        if args[1] == "-np":
+            output_data = False
+
     root_dir = input("Enter path:")
+    root_dir = root_dir if root_dir != "" else os.getcwd()
      
     if os.path.exists(root_dir):
-        print("OK")
-        tree = Tree(root_dir)
+        print(f"OK, using path: {root_dir}")
+        
+        hidden_allowed = confirmed_input("Search hidden files/dirs? (y/n): ", ["y", "n"])
+        hidden_allowed = True if hidden_allowed == "y" else False
+        
+        Tree(root_dir, hidden_allowed, output_data)
+        
     else:
         print("FAILED")
 
-def test_scanner():
-    scanner = Scanner()
-    scanner.add_flags()
-
 
 if __name__ == '__main__':
-    test_scanner()
+	main()
